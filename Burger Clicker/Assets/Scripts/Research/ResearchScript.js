b7088631203c32d17e12bc77cd4468c446dbb791
IWillRekU3206 <iwillreku3206@gmail.com>
2019-01-16 19:52:42 +0800

    started research feature
    note to self: use json for research storage

---
+++ Burger Clicker/Assets/Scripts/Research/ResearchScript.js
@@ -0,0 +1,32 @@
+// This script shall not be copied for any commercial or hobbyist use, unless permitted by the creator, IWillRekU3206
+
+#pragma strict
+
+var rNames: String[] = [
+	"",
+	"Upgraded Burger Machine I",
+	"Upgraded Burger Machine II",
+	"Upgraded Burger Machine III",
+	"Upgraded Burger Machine IV",
+	"Upgraded Burger Machine V",
+	"Upgraded Burger Machine VI",
+	"Upgraded Burger Machine VII",
+	"Upgraded Burger Machine VIII",
+	"Upgraded Burger Machine IX",
+	"Upgraded Burger Machine X"
+];
+
+function Start ()
+{
+
+}
+
+function Update ()
+{
+
+}
+
+function FixedUpdate ()
+{
+
+}
---
+++ Burger Clicker/Assets/script template.js
@@ -0,0 +1,18 @@
+// This script shall not be copied for any commercial or hobbyist use, unless permitted by the creator, IWillRekU3206
+
+#pragma strict
+
+function Start ()
+{
+	
+}
+
+function Update ()
+{
+	
+}
+
+function FixedUpdate ()
+{
+	
+}
