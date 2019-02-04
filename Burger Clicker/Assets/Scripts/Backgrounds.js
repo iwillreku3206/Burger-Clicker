8963e2fab346e28f499d4739aca3fa52dd2fc13e
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2019-02-04 12:11:05 +0800

    Update Backgrounds.js

--- Burger Clicker/Assets/Scripts/Backgrounds.js
+++ Burger Clicker/Assets/Scripts/Backgrounds.js
@@ -7,7 +7,17 @@ var t_Restaurant: Texture;
 
 var Background: RawImage;
 
+var GameHandler: GameHandler;
+
 function Start(){bgDefault();}
+
+function Update()
+{
+  if (GameHandler.prestiges==0){bgDefault();}
+  if (GameHandler.prestiges==1){bgTruck();}
+  if (GameHandler.prestiges==2){bgStall();}
+  if (GameHandler.prestiges==3){bgRestaurant();}
+}
 
 function bgDefault(){Background.texture = t_Default;}
 function bgTruck(){Background.texture = t_Truck;}
