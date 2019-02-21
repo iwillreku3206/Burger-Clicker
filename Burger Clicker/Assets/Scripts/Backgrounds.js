43b0f5207d51c34474333629639196f7ad18fb6f
IWillRekU3206 <iwillreku3206@gmail.com>
2019-02-21 21:23:20 +0800

    added backgrounds
    this will probably be the last update to the game

--- Burger Clicker/Assets/Scripts/Backgrounds.js
+++ Burger Clicker/Assets/Scripts/Backgrounds.js
@@ -2,7 +2,6 @@
 
 var t_Default: Texture;
 var t_Truck: Texture;
-var t_Stall: Texture;
 var t_Restaurant: Texture;
 
 var Background: RawImage;
@@ -15,11 +14,10 @@ function Update()
 {
   if (GameHandler.prestiges==0){bgDefault();}
   if (GameHandler.prestiges==1){bgTruck();}
-  if (GameHandler.prestiges==2){bgStall();}
-  if (GameHandler.prestiges==3){bgRestaurant();}
+  if (GameHandler.prestiges>1){bgRestaurant();}
+
 }
 
 function bgDefault(){Background.texture = t_Default;}
 function bgTruck(){Background.texture = t_Truck;}
-function bgStall(){Background.texture = t_Stall;}
 function bgRestaurant(){Background.texture = t_Restaurant;}
